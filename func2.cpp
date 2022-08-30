d00b17594da37ee7a5f9d3c54470d89bdb8a0150
Giovanni Bussi <giovanni.bussi@gmail.com>
2022-08-30 18:00:51 +0200

    more similar

--- func2.cpp
+++ func2.cpp
@@ -1,6 +1,19 @@
+#include <string>
+#include <sstream>
+
 double scale1(){
   return 1.0;
 }
 double scale2(){
   return 1.0;
 }
+
+bool convertToAny(const std::string & str,double & t) {
+  std::istringstream istr(str.c_str());
+  bool ok=static_cast<bool>(istr>>t);
+  if(!ok) return false;
+  std::string remaining;
+  istr>>remaining;
+  return remaining.length()==0;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <cstdio>
+#include <string>
 
 double compute(int n,double dt);
 
 double getDt(double dt);
 
+bool convertToAny(const std::string & str,double & t);
+
 int main(){
   double dt;
   int n;
-  dt=getDt(0.005);
+  convertToAny("0.005",dt);
+  dt=getDt(dt);
   for(int n=0;n<1000;n+=50) {
     std::printf("%f\n",compute(n,dt));
   }
